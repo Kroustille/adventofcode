a6b135f5cc90da8ce69c4787cb0e7f23c3adfb65
Kroustille <valentins@hey.com>
2021-12-15 22:05:26 +0100

    feat: resolve part 2 of challenge 13 of 2021

--- 2021/13/challenge/index.go
+++ 2021/13/challenge/index.go
@@ -1,4 +1,49 @@
 package challenge
 
+import "fmt"
+
 type Challenge struct {
+}
+
+type Coordinates struct {
+	x int
+	y int
+}
+
+func (c Coordinates) Key() string {
+	return fmt.Sprintf("%d,%d", c.x, c.y)
+}
+
+func (c Challenge) FoldAlongX(all_coordinates map[string]Coordinates, fold_coordinate int) map[string]Coordinates {
+	new_coordinates := make(map[string]Coordinates, 0)
+	for _, coordinate := range all_coordinates {
+		if coordinate.x < fold_coordinate {
+			new_coordinates[coordinate.Key()] = coordinate
+		} else {
+			folded_coordinate := Coordinates{
+				x: fold_coordinate - (coordinate.x - fold_coordinate),
+				y: coordinate.y,
+			}
+			new_coordinates[folded_coordinate.Key()] = folded_coordinate
+		}
+	}
+
+	return new_coordinates
+}
+
+func (c Challenge) FoldAlongY(all_coordinates map[string]Coordinates, fold_coordinate int) map[string]Coordinates {
+	new_coordinates := make(map[string]Coordinates, 0)
+	for _, coordinate := range all_coordinates {
+		if coordinate.y < fold_coordinate {
+			new_coordinates[coordinate.Key()] = coordinate
+		} else {
+			folded_coordinate := Coordinates{
+				x: coordinate.x,
+				y: fold_coordinate - (coordinate.y - fold_coordinate),
+			}
+			new_coordinates[folded_coordinate.Key()] = folded_coordinate
+		}
+	}
+
+	return new_coordinates
 }
--- 2021/13/challenge/part_2.go
+++ 2021/13/challenge/part_2.go
@@ -1,12 +1,76 @@
 package challenge
 
 import (
+	"log"
+	"strings"
 	"time"
+
+	"github.com/Kroustille/adventofcode/utils"
 )
 
 func (c Challenge) ResolvePart2(lines []string) (int, time.Duration) {
 	start := time.Now()
 
-	result := 2
+	all_coordinates := make(map[string]Coordinates, 0)
+	fold_start_index := 0
+	max_x := 0
+	max_y := 0
+	for i, line := range lines {
+		if line == "" {
+			fold_start_index = i + 1
+			break
+		}
+		splitted_line := strings.Split(line, ",")
+		new_coordinates := Coordinates{
+			x: utils.FatalReadInt(splitted_line[0]),
+			y: utils.FatalReadInt(splitted_line[1]),
+		}
+
+		if new_coordinates.x > max_x {
+			max_x = new_coordinates.x
+		}
+
+		if new_coordinates.y > max_y {
+			max_y = new_coordinates.y
+		}
+
+		all_coordinates[new_coordinates.Key()] = new_coordinates
+	}
+
+	for i := fold_start_index; i < len(lines); i++ {
+		log.Println(lines[i])
+		splitted_line := strings.Split(lines[i], " ")
+		fold_instructions := strings.Split(splitted_line[2], "=")
+		coordinate_type := fold_instructions[0]
+		coordinate_value := utils.FatalReadInt(fold_instructions[1])
+		if coordinate_type == "x" {
+			all_coordinates = c.FoldAlongX(all_coordinates, coordinate_value)
+			max_x /= 2
+		}
+
+		if coordinate_type == "y" {
+			all_coordinates = c.FoldAlongY(all_coordinates, coordinate_value)
+			max_y /= 2
+		}
+	}
+
+	for y := 0; y < max_y; y++ {
+		display_line := ""
+		for x := 0; x < max_x; x++ {
+			coordinate := Coordinates{
+				x: x,
+				y: y,
+			}
+			if _, ok := all_coordinates[coordinate.Key()]; ok {
+				display_line += "#"
+			} else {
+				display_line += "."
+			}
+		}
+		log.Println(display_line)
+	}
+
+	result := len(all_coordinates)
+
 	return result, time.Since(start)
 }
